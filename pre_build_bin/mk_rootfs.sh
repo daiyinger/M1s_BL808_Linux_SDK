0aebbf8e9ea447751ca646bea49cc504ef1d4a73
daiyinger <1002639334@qq.com>
2024-02-19 04:02:33 -0800

    optimize make rootfs

--- pre_build_bin/mk_rootfs.sh
+++ pre_build_bin/mk_rootfs.sh
@@ -6,6 +6,19 @@ if [ -f $xram_net_path ]; then
 	cp $xram_net_path rootfs/lib/modules/
 fi
 
+if [ ! -d rootfs/dev ]; then
+	mkdir rootfs/dev
+	cd rootfs/dev
+	sudo mknod -m 660 console c 5 1
+	sudo mknod -m 660 null c 1 3
+	mkdir pts
+	cd -
+fi
+
+if [ ! -d rootfs/proc ]; then
+	mkdir rootfs/proc
+fi
+
 genext2fs -b 8192 -d rootfs ramdisk
 
 rm -f  ramdisk.lz4
